e89864fb946bec08df19bebb26ec1bd669835c12
Russell Barnes <russellbarnes18@gmail.com>
2022-04-26 05:44:20 +0800

    Used PUT and DELETE REQUEST

--- myAPI/serializers.py
+++ myAPI/serializers.py
@@ -4,4 +4,4 @@ from .models import vehicles
 class vehiclesSerializer (serializers.HyperlinkedModelSerializer):
     class Meta:
         model = vehicles
-        fields = ('name','description')
+        fields = ('id','name','description')
